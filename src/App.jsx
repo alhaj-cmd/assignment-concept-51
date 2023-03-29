5897bff7922fd6a004b20c59935357204787105b
github_username <you@domain.com>
2023-03-29 20:55:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from './component/Header/Header';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Header></Header>
+     </div>
+  )
+}
+
+export default App
